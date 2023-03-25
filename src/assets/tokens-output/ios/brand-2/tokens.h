b6633302f6ef3d91f69da203c3ce748bcd4dedf4
Steven Aerts <stevenaerts@me.com>
2023-03-25 20:59:18 +0100

    go

--- src/assets/tokens-output/ios/brand-2/tokens.h
+++ src/assets/tokens-output/ios/brand-2/tokens.h
@@ -4,33 +4,23 @@
 //
 
 // Do not edit directly
-// Generated on Sat, 25 Mar 2023 19:09:47 GMT
+// Generated on Sat, 25 Mar 2023 19:59:14 GMT
 
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-#define FontFontFamiliesPoppins @"Poppins"
-#define FontLineHeights0 @"120%"
-#define FontLineHeights1 @"140%"
-#define FontFontWeightsPoppins0 @"SemiBold"
-#define FontFontWeightsPoppins1 @"Bold"
-#define FontFontSize0 @"20"
-#define FontFontSize1 @"24"
-#define FontLetterSpacing0 @"0%"
-#define FontParagraphSpacing0 @"0"
-#define FontTextCaseNone @"none"
-#define FontTextDecorationNone @"none"
-#define FontParagraphIndent0 @"0px"
-#define FontPlatformSystem @"San Francisco"
-#define SizeFontSmall 224.00f
-#define SizeFontMedium 16.00f
-#define SizeFontLarge 32.00f
-#define SizeFontBase 16.00f
-#define SpacingXs 4px
-#define SpacingS 8
-#define SpacingM 16
-#define SpacingL 8*2
-#define SpacingXl 16*2
-#define ButtonHeight 50px
+#define ColorBrandPrimary [UIColor colorWithRed:0.847f green:0.094f blue:0.976f alpha:1.000f]
+#define ColorBrandSecondary [UIColor colorWithRed:0.459f green:0.024f blue:0.510f alpha:1.000f]
+#define ColorAccentNeutral10 [UIColor colorWithRed:0.945f green:0.945f blue:0.945f alpha:1.000f]
+#define ColorAccentNeutral20 [UIColor colorWithRed:0.851f green:0.851f blue:0.851f alpha:1.000f]
+#define ColorAccentNeutral30 [UIColor colorWithRed:0.757f green:0.757f blue:0.757f alpha:1.000f]
+#define ColorAccentNeutral40 [UIColor colorWithRed:0.663f green:0.663f blue:0.663f alpha:1.000f]
+#define ColorAccentNeutral50 [UIColor colorWithRed:0.569f green:0.569f blue:0.569f alpha:1.000f]
+#define ColorAccentNeutral60 [UIColor colorWithRed:0.475f green:0.475f blue:0.475f alpha:1.000f]
+#define ColorAccentNeutral70 [UIColor colorWithRed:0.376f green:0.376f blue:0.376f alpha:1.000f]
+#define ColorAccentNeutral80 [UIColor colorWithRed:0.282f green:0.282f blue:0.282f alpha:1.000f]
+#define ColorAccentNeutral90 [UIColor colorWithRed:0.188f green:0.188f blue:0.188f alpha:1.000f]
+#define ColorAccentNeutral100 [UIColor colorWithRed:0.094f green:0.094f blue:0.094f alpha:1.000f]
+#define ColorAccentBackground0 [UIColor colorWithRed:0.937f green:0.890f blue:0.953f alpha:1.000f]
 
