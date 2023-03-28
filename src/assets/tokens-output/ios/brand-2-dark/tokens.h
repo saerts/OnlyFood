ebe9b4d69d60c805db8d267f8adaf35c22dc5acd
Steven Aerts <stevenaerts@me.com>
2023-03-28 09:08:39 +0200

    borders 2b

--- src/assets/tokens-output/ios/brand-2-dark/tokens.h
+++ src/assets/tokens-output/ios/brand-2-dark/tokens.h
@@ -4,12 +4,49 @@
 //
 
 // Do not edit directly
-// Generated on Mon, 27 Mar 2023 18:28:24 GMT
+// Generated on Tue, 28 Mar 2023 07:07:49 GMT
 
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+#define ColorBrandPrimary50 [UIColor colorWithRed:0.984f green:0.910f blue:0.996f alpha:1.000f]
+#define ColorBrandPrimary100 [UIColor colorWithRed:0.953f green:0.718f blue:0.992f alpha:1.000f]
+#define ColorBrandPrimary200 [UIColor colorWithRed:0.929f green:0.584f blue:0.988f alpha:1.000f]
+#define ColorBrandPrimary300 [UIColor colorWithRed:0.898f green:0.392f blue:0.984f alpha:1.000f]
+#define ColorBrandPrimary400 [UIColor colorWithRed:0.878f green:0.275f blue:0.980f alpha:1.000f]
+#define ColorBrandPrimary500 [UIColor colorWithRed:0.847f green:0.094f blue:0.976f alpha:1.000f]
+#define ColorBrandPrimary600 [UIColor colorWithRed:0.773f green:0.086f blue:0.890f alpha:1.000f]
+#define ColorBrandPrimary700 [UIColor colorWithRed:0.600f green:0.067f blue:0.694f alpha:1.000f]
+#define ColorBrandPrimary800 [UIColor colorWithRed:0.467f green:0.051f blue:0.537f alpha:1.000f]
+#define ColorBrandPrimary900 [UIColor colorWithRed:0.357f green:0.039f blue:0.412f alpha:1.000f]
+#define ColorBrandSecondary50 [UIColor colorWithRed:0.945f green:0.902f blue:0.953f alpha:1.000f]
+#define ColorBrandSecondary100 [UIColor colorWithRed:0.831f green:0.698f blue:0.847f alpha:1.000f]
+#define ColorBrandSecondary200 [UIColor colorWithRed:0.753f green:0.549f blue:0.776f alpha:1.000f]
+#define ColorBrandSecondary300 [UIColor colorWithRed:0.639f green:0.345f blue:0.671f alpha:1.000f]
+#define ColorBrandSecondary400 [UIColor colorWithRed:0.569f green:0.220f blue:0.608f alpha:1.000f]
+#define ColorBrandSecondary500 [UIColor colorWithRed:0.459f green:0.024f blue:0.510f alpha:1.000f]
+#define ColorBrandSecondary600 [UIColor colorWithRed:0.416f green:0.020f blue:0.463f alpha:1.000f]
+#define ColorBrandSecondary700 [UIColor colorWithRed:0.325f green:0.016f blue:0.361f alpha:1.000f]
+#define ColorBrandSecondary800 [UIColor colorWithRed:0.251f green:0.012f blue:0.282f alpha:1.000f]
+#define ColorBrandSecondary900 [UIColor colorWithRed:0.192f green:0.012f blue:0.216f alpha:1.000f]
+#define ColorAccentNeutral10 [UIColor colorWithRed:0.094f green:0.094f blue:0.094f alpha:1.000f]
+#define ColorAccentNeutral20 [UIColor colorWithRed:0.188f green:0.188f blue:0.188f alpha:1.000f]
+#define ColorAccentNeutral30 [UIColor colorWithRed:0.282f green:0.282f blue:0.282f alpha:1.000f]
+#define ColorAccentNeutral40 [UIColor colorWithRed:0.376f green:0.376f blue:0.376f alpha:1.000f]
+#define ColorAccentNeutral50 [UIColor colorWithRed:0.475f green:0.475f blue:0.475f alpha:1.000f]
+#define ColorAccentNeutral60 [UIColor colorWithRed:0.569f green:0.569f blue:0.569f alpha:1.000f]
+#define ColorAccentNeutral70 [UIColor colorWithRed:0.663f green:0.663f blue:0.663f alpha:1.000f]
+#define ColorAccentNeutral80 [UIColor colorWithRed:0.757f green:0.757f blue:0.757f alpha:1.000f]
+#define ColorAccentNeutral90 [UIColor colorWithRed:0.851f green:0.851f blue:0.851f alpha:1.000f]
+#define ColorAccentNeutral100 [UIColor colorWithRed:0.945f green:0.945f blue:0.945f alpha:1.000f]
+#define ColorAccentBackground0 [UIColor colorWithRed:0.000f green:0.000f blue:0.000f alpha:1.000f]
+#define S 1
+#define M 2
+#define L 4
+#define Bodytext [UIColor colorWithRed:0.851f green:0.851f blue:0.851f alpha:1.000f]
+#define Primary [UIColor colorWithRed:0.847f green:0.094f blue:0.976f alpha:1.000f]
+#define Secondary [UIColor colorWithRed:0.459f green:0.024f blue:0.510f alpha:1.000f]
 #define RadiusXs 4px
 #define RadiusS 8px
 #define RadiusM 16px
--- src/assets/tokens-output/web/brand-1/variables.js
+++ src/assets/tokens-output/web/brand-1/variables.js
@@ -1,8 +1,45 @@
 /**
  * Do not edit directly
- * Generated on Mon, 27 Mar 2023 18:28:24 GMT
+ * Generated on Tue, 28 Mar 2023 07:07:49 GMT
  */
 
+export const ColorBrandPrimary50 = "#ebeef5";
+export const ColorBrandPrimary100 = "#c2ccdf";
+export const ColorBrandPrimary200 = "#a5b3d0";
+export const ColorBrandPrimary300 = "#7c90ba";
+export const ColorBrandPrimary400 = "#627aad";
+export const ColorBrandPrimary500 = "#3b5998";
+export const ColorBrandPrimary600 = "#36518a";
+export const ColorBrandPrimary700 = "#2a3f6c";
+export const ColorBrandPrimary800 = "#203154";
+export const ColorBrandPrimary900 = "#192540";
+export const ColorBrandSecondary50 = "#f3f7ff";
+export const ColorBrandSecondary100 = "#dbe7ff";
+export const ColorBrandSecondary200 = "#c9dbff";
+export const ColorBrandSecondary300 = "#b1cbff";
+export const ColorBrandSecondary400 = "#a1c1ff";
+export const ColorBrandSecondary500 = "#8ab1ff";
+export const ColorBrandSecondary600 = "#7ea1e8";
+export const ColorBrandSecondary700 = "#627eb5";
+export const ColorBrandSecondary800 = "#4c618c";
+export const ColorBrandSecondary900 = "#4c618c";
+export const ColorAccentNeutral10 = "#f1f1f1";
+export const ColorAccentNeutral20 = "#d9d9d9";
+export const ColorAccentNeutral30 = "#c1c1c1";
+export const ColorAccentNeutral40 = "#a9a9a9";
+export const ColorAccentNeutral50 = "#919191";
+export const ColorAccentNeutral60 = "#797979";
+export const ColorAccentNeutral70 = "#606060";
+export const ColorAccentNeutral80 = "#484848";
+export const ColorAccentNeutral90 = "#303030";
+export const ColorAccentNeutral100 = "#181818";
+export const ColorAccentBackground0 = "#eff6f9";
+export const S = "1";
+export const M = "2";
+export const L = "4";
+export const Bodytext = "#303030";
+export const Primary = "#3b5998";
+export const Secondary = "#8ab1ff";
 export const RadiusXs = "4px";
 export const RadiusS = "8px";
 export const RadiusM = "16px";
